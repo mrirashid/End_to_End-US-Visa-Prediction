711a6e8fd4e8036f770e49bca3b15fc37d6d4c9d
mri rashid <mdislam0996@gmail.com>
2025-10-27 12:55:21 +0600

    data added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa",
+    version="0.1",
+    author="Md Rashidul Islam",
+    author_email="mdislam0996@gmail.com",
+    packages=find_packages()
+
+)
